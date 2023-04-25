7bd3a77d68c0e69448ed2bb9b4a28a4a7a273df2
Kamil Wiater <kamilwiater.uj@gmail.com>
2023-04-25 14:51:22 +0200

    HTML+SCSS+Setuup_Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navigation from "./components/Navigation/Navigation";
+import Noun from "./components/Noun/Noun";
+import Verb from "./components/Verb/Verb";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <div className="main-container">
         <Navigation />
         <Hero />
+        <Noun />
+        <Verb />
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+export default function Footer() {
+  return (
+    <section className="footer">
+      <h4>Source</h4>
+      <p className="source">https://en.wiktionary.org/wiki/keyboard</p>
+      <img src="src/assets/images/icon-new-window.svg" alt="open-source" />
+    </section>
+  );
+}
