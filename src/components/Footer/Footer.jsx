6db6729fec8cea2e6d0dee4d3ea4a2b1ab3867fc
Kamil Wiater <kamilwiater.uj@gmail.com>
2023-04-26 15:48:26 +0200

    RWD+Active_States_Completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,11 @@ export default function Footer({ outcome }) {
     return (
       <section className="footer">
         <h4>Source</h4>
-        <p className="source">{outcome[0].sourceUrls[0]}</p>
-        <img src="src/assets/images/icon-new-window.svg" alt="open-source" />
+        <a href={outcome[0].sourceUrls[0]} target="_blank" className="source">
+          {outcome[0].sourceUrls[0]}
+          <img src="src/assets/images/icon-new-window.svg" alt="open-source" />
+        </a>
+        
       </section>
     );
   }
--- src/components/Synonyms/Synonyms.jsx
+++ src/components/Synonyms/Synonyms.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Synonyms({ item }) {
   if (item.synonyms.length > 0) {
     return (
-      <h3>
-        Synonims
-        <span className="synonyms"> {item.synonyms.join(", ")}</span>
-      </h3>
+      <div className="synonyms">
+        <h3>Synonims</h3>
+        <h3 className="h3-synonyms"> {item.synonyms.join(", ")}</h3>
+      </div>
     );
   }
 }
